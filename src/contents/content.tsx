0c7014ff2644f89c7013f8eb7fff031e4c16abd9
naritaka1192 <151434061+naritaka1192@users.noreply.github.com>
2024-05-05 13:03:17 +0900

    項目追加

--- src/contents/content.tsx
+++ src/contents/content.tsx
@@ -7,13 +7,12 @@ const Content = () => {
     <div className="App">
       <Grid container>
         <Grid item>
-      <h1>【表題】</h1>
+      <h1>建築一式工事業</h1>
       <ul className="list">
-        <li>こういう事がやりたい</li>
-        <li style={{color:'red',fontWeight:'bold'}}>fafafa</li>
-        <li>だができない</li>
-        <li>だができない</li>
-
+        <li>総合的な企画、指導、調整のもとに建築物を建設する工事</li>
+        <li>基本的に一式工事とされる大規模、複雑な工事等が該当</li>
+        <li>電気工事、管工事、内装仕上工事等の単一工事であっても、<br/>工事の規模、複雑性等からみて個別の専門工事として<br/>施工することが困難なものは建築一式工事に含まれる</li>
+        <li></li>
       </ul>
       </Grid>
       <Grid item>
